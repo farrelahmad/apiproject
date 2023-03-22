2e9841692e8eae36f2126584308c02204fc4ade4
FarrelAhmad <farrel.aurell@gmail.com>
2023-03-22 11:40:02 +0700

    test get stories

--- app/Http/Controllers/StorieController.php
+++ app/Http/Controllers/StorieController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Storie;
 use Illuminate\Http\Request;
 
 class StorieController extends Controller
 {
-    //
+    public function index(){
+        $storie = Storie::all();
+        return response()->json($storie);
+    }   
 }
--- app/Models/Storie.php
+++ app/Models/Storie.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Storie extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'title',
+        'stories',
+        'storiewriter',
+    ];
 }
